b45ad64156dd61859c6623914205ef47f3372a0d
azizxojaev <azizxojaevbexruz@gmail.com>
2024-04-07 13:26:18 +0500

    added contact model

--- east_to_west/models.py
+++ east_to_west/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 
+class Contact(models.Model):
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    location = models.CharField(max_length=150)
+    working_days = models.CharField(max_length=20)
+    working_hours = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = 'Контактная информация'
+        verbose_name_plural = 'Контактная информация'
+
+    def __str__(self):
+        return 'Контактная информация'
--- east_to_west/views.py
+++ east_to_west/views.py
@@ -1,33 +1,50 @@
 from django.shortcuts import render
+from .models import *
 
 
 def home_page(request):
-    return render(request, 'index.html')
+    contact = Contact.objects.first()
+    context = {'contact': contact}
+    return render(request, 'index.html', context=context)
 
 
 def about_page(request):
-    return render(request, 'about.html')
+    contact = Contact.objects.first()
+    context = {'contact': contact}
+    return render(request, 'about.html', context=context)
 
 
 def contact_page(request):
-    return render(request, 'contact-us.html')
+    contact = Contact.objects.first()
+    context = {'contact': contact}
+    return render(request, 'contact-us.html', context=context)
 
 
 def tours_page(request):
-    return render(request, 'tour-grid.html')
+    contact = Contact.objects.first()
+    context = {'contact': contact}
+    return render(request, 'tour-grid.html', context=context)
 
 
 def tour_detail_page(request):
-    return render(request, 'tour-detail.html')
+    contact = Contact.objects.first()
+    context = {'contact': contact}
+    return render(request, 'tour-detail.html', context=context)
 
 
 def tour_booking_page(request):
-    return render(request, 'tour-booking.html')
+    contact = Contact.objects.first()
+    context = {'contact': contact}
+    return render(request, 'tour-booking.html', context=context)
 
 
 def blogs_page(request):
-    return render(request, 'blog-grid.html')
+    contact = Contact.objects.first()
+    context = {'contact': contact}
+    return render(request, 'blog-grid.html', context=context)
 
 
 def blog_detail_page(request):
-    return render(request, 'blog-detail.html')
+    contact = Contact.objects.first()
+    context = {'contact': contact}
+    return render(request, 'blog-detail.html', context=context)
