9edfa5df54ce60323b3a5904bdf7967b47075d54
azizxojaev <azizxojaevbexruz@gmail.com>
2024-04-11 17:43:31 +0500

    started country house booking

--- tours/models.py
+++ tours/models.py
@@ -18,3 +18,35 @@ class Departure(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Departure, self).save(*args, **kwargs)
+
+
+class CountryHouse(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=150)
+    price = models.FloatField()
+    video = models.URLField()
+    description = models.TextField()
+    location = models.CharField(max_length=300)
+    capacity = models.IntegerField()
+    rooms = models.CharField(max_length=200)
+    have_tv = models.BooleanField()
+    have_playstation = models.BooleanField()
+    have_karaoke = models.BooleanField()
+    have_tenis = models.BooleanField()
+    have_tapchan = models.BooleanField()
+    pool = models.CharField(max_length=150)
+    have_sauna = models.BooleanField()
+    have_terrace = models.BooleanField()
+    phone_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(CountryHouse, self).save(*args, **kwargs)
+
+
+class HouseImage(models.Model):
+    image = models.ImageField(upload_to='houses/')
+    house = models.ForeignKey(CountryHouse, on_delete=models.CASCADE)
--- tours/urls.py
+++ tours/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('tour-booking/', tour_booking_page, name='tour_booking'),
+    path('country-houses/', country_houses_page, name='country_houses'),
+    path('tours/<str:slug>', country_house_detail_page, name='country_house_detail'),
 ]
