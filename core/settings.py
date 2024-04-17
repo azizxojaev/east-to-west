458774b3790c6d5d50508804d36ccaabb5c3651b
azizxojaev <azizxojaevbexruz@gmail.com>
2024-04-17 14:28:39 +0500

    fixed csrf error

--- core/settings.py
+++ core/settings.py
@@ -151,4 +151,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CSRF_TRUSTED_ORIGINS = ['https://east2west.uz/']
+CSRF_TRUSTED_ORIGINS = ['https://east2west.uz']
